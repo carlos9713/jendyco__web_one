67f5aebc82b23c3399c7811b2d1fd8708ec48ce8
Carlos Castillo <carlos.castillo9713@gmail.com>
2025-07-25 06:32:38 -0400

    Fix: wrap Routes in BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Products from "./pages/Products";
@@ -8,12 +8,14 @@ import About from "./pages/About";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/quote" element={<Quote />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
