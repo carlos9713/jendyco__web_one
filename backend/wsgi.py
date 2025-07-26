404da15f949a297392a8a035edc04095e07992f2
Carlos Castillo <carlos.castillo9713@gmail.com>
2025-07-26 05:09:01 -0400

    Fix wsgi and manage.py settings module

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,8 +1,8 @@
-import sys
 import os
+import sys
 
-# Add the parent folder of backend (project root) to sys.path
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Add the backend directory to the python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
